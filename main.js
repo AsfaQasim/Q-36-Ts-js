f1699fa0d01477eafe35e588c856b5f7d2b596f4
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 09:06:41 -0800

    Q 36 of Ts/js & node js of 45 assignment.
    Q 36 of Ts/js & node js of 45 assignment.

---
+++ main.js
@@ -0,0 +1,5 @@
+function make_Shirt(size, message) {
+    console.log(" You order a ".concat(size, " shirt that says: \"").concat(message, "\""));
+}
+make_Shirt("large", "I love tpescript");
+make_Shirt("medium", "I need a big shirt");
---
+++ main.ts
@@ -0,0 +1,6 @@
+function make_Shirt(size: string,message: string): void{
+    console.log(` You order a ${size} shirt that says: "${message}"`);
+    
+}
+make_Shirt(`large` ,"I love tpescript")
+make_Shirt(`medium`,"I need a big shirt");
